626776e1f4716c4d1affd979c9b773806f644241
Max Hammond <max.hammond21@gmail.com>
2023-06-16 13:54:48 +1000

    intial commit

---
+++ src/wserver.c
@@ -0,0 +1,12 @@
+#include "wserver.h"
+
+int main(int argc, char **argv) {
+    // if (argc != 4) {
+    //     printf("Incorrect usage\n");
+    //     printf("Should be ./wserver <ipv4> <port> <root dir>\n");
+    //     return 1;
+    // }
+    printf("Hello, world\n");
+    
+    return 0;
+}
